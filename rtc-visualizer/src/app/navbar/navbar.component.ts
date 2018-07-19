6a6cc6360d03fbf1dfe503e64c4cb2cddc5ecfcb
Scorbutics <brice.bulgarelli@outlook.com>
2018-07-20 00:11:26 +0200

    Feat : Amélioration du look de la nav-bar et ajout des boutons

--- rtc-visualizer/src/app/navbar/navbar.component.ts
+++ rtc-visualizer/src/app/navbar/navbar.component.ts
@@ -3,18 +3,17 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 @Component({
 	selector: 'nav-bar',
 	templateUrl: './navbar.component.html',
-	styleUrls: []
+	styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-	height:number;
+	height:number = 70;
 	collapse:boolean = true;
 	
 	constructor(private navbarElement:ElementRef) {
 	}
 
 	ngOnInit() { 
-		this.height = this.navbarElement.nativeElement.offsetHeight - this.navbarElement.nativeElement.offsetTop;
 	}
 
 }
