9a2fe04d7a44b2a9164212545064971c0a2002f8
Scorbutics <brice.bulgarelli@outlook.com>
2018-07-21 15:15:45 +0200

    Feat : mise en place du routing

--- rtc-visualizer/src/app/app.module.ts
+++ rtc-visualizer/src/app/app.module.ts
@@ -1,22 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Routes, RouterModule } from "@angular/router";
 
+//User defined
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { UsPageComponent } from './pages/usPage/usPage.component';
 import { NamedBlockComponent } from './blocks/namedBlock.component';
+import { PageTitleComponent } from './pages/pageTitle/pageTitle.component';
+import { PiPageComponent } from './pages/piPage/piPage.component';
+
+const routes: Routes = [
+	{ path: '', redirectTo: 'login', pathMatch: 'full' },
+	{ path: '**', redirectTo: 'login'},
+	
+	//TODO
+	{ path: 'login', redirectTo: 'us'},
+
+	{ path: 'us', component: UsPageComponent },
+	{ path: 'pi', component: PiPageComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    UsPageComponent,
-    NamedBlockComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NavbarComponent,
+		PageTitleComponent,
+		UsPageComponent,
+		PiPageComponent,
+		NamedBlockComponent
+	],
+	imports: [
+		NgbModule.forRoot(),
+		RouterModule.forRoot(routes),
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ rtc-visualizer/src/app/pages/piPage/piPage.component.ts
@@ -0,0 +1,10 @@
+import { Component  } from '@angular/core';
+
+@Component({
+	selector: 'pi-page',
+	templateUrl: './piPage.component.html',
+	styleUrls: ['./piPage.component.css']
+})
+export class PiPageComponent {
+	piName:string = "PI Eté";
+}
